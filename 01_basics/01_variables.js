a910f4a24a3240cecc4eb17fc66e7ac00abacbe4
Akash pandey <akashpandey1612@gmail.com>
2026-09-14 16:19:04 +0000

    "add account variables and console.table".

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "akashpandey1612@gmail.com"
+var accountPassword = "12345" 
+accountCity = "SDDN" 
+let accountstate ;
+
+// accountId = 144553 not allowed
+accountEmail = "lal1612@gmail.com"
+accountPassword = "345" 
+accountCity = "Gkp" 
+
+// console.log(accountId) ;
+
+console.table([accountId , accountEmail , accountCity , accountPassword , accountstate])
