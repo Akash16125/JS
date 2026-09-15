8550538b1f7830c8bc178bce9fcc1ccd3279cc9a
Akash pandey <akashpandey1612@gmail.com>
2026-09-15 16:22:49 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" ; /* treat all js code as newer version*/
+
+// alert(3+3) we are using nodejs , not browser
+
+let name = "Akash"
+
+let age = 19 
+
+let isloggedIn = false 
+
+
+let state ;
+
+// number =>2 to power 53
+// bigint
+// string => " "
+// boolean  => true/false    
+// null => standalone value
+// undefined => 
+// symbol
+
+
+console.log(typeof age) ;
