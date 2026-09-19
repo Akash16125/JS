a77a5c676dfe771d45e6e9ad7b2a463b0c669329
Akash pandey <akashpandey1612@gmail.com>
2026-09-19 05:01:54 +0000

    data conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,41 @@
+// let score = "33abc" 
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInnumber = Number(score) 
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber)
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1 , false => 0
+
+// let isLoggedIn = 1 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let someNumber = 33
+
+// let StringNumber = String(someNumber) 
+// console.log(StringNumber);
+// console.log(typeof (StringNumber));
+
+
+//*****Operations ************** */
+
+// let value = 3 ;
+// let negvalue = -value ;
+// // console.log(negvalue) ;
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+
+// let str1 = "Hello" ;
+// let str2 = "Akash" ;
+// let str3 = str1 + str2 ;
+
+// console.log(str3)
+//
